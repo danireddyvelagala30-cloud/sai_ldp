67e7eee2ef5de2515590a4166f423e1d487c68b6
Sai <danireddy.velagala30@gmail.com>
2026-07-27 10:14:34 +0530

    Update email resolution and refactor display function

--- My-Portfolio/myassignment/myassignment/promises.js
+++ My-Portfolio/myassignment/myassignment/promises.js
@@ -2,18 +2,16 @@ const getData = (uId) => {
     return new Promise((resolve) => {
         setTimeout(() => {
             console.log("Fetched the data!");
-            resolve("skc@gmail.com");
+            resolve(`User ${uId}: skc@gmail.com`);
         }, 4000);
     });
 };
 
-const displayEmail = async () => {
-    console.log("Start");
-
+const main = async () => {
+    console.log("start");
     const email = await getData("skc");
-
-    console.log("Email id of the user is: " + email);
-
-    console.log("End");
+    console.log("Email id of the user id is: " + email);
+    console.log("end");
 };
-displayEmail();
+
+main();
