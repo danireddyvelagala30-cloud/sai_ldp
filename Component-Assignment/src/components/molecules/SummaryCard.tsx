21c59d713f6e48821e171c8b10fd4825dd1d41fa
Sai Reddy <danireddy.velagala30@gmail.com>
2026-08-03 01:07:19 +0530

    Adding Component Assignment

---
+++ Component-Assignment/src/components/molecules/SummaryCard.tsx
@@ -0,0 +1,25 @@
+import TextAtom from "../atoms/TextAtom";
+
+const SummaryCard = () => {
+  return (
+    <div className="summary">
+      <h2>Summary</h2>
+
+      <p>
+        <TextAtom text="Term: 12 Months" />
+      </p>
+
+      <p>
+        <TextAtom text="Selected Contracts: 0" />
+      </p>
+
+      <p>
+        <TextAtom text="Total Payable: $0.00" />
+      </p>
+
+      <button>Review Your Credit</button>
+    </div>
+  );
+};
+
+export default SummaryCard;
