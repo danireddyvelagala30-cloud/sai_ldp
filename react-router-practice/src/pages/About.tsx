86718c83790fd5b404fc639363bfeb2bc21472fb
Sai Reddy <danireddy.velagala30@gmail.com>
2026-08-16 18:39:27 +0530

    practice for the react router

---
+++ react-router-practice/src/pages/About.tsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This application is built using React and React Router.</p>
+    </div>
+  );
+};
+
+export default About;
